530695921cfa1d31695af1575cde9184da99222a
DanielvdSpoel <applenotch2@gmail.com>
2017-10-31 11:58:27 +0100

    Added option for custom mod log channel

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const Discord = require('discord.js'); // Gets The Discord.js Package
 const fs = require("fs"); // Gets the fs Package
 const bot = new Discord.Client(); // Our Discord Client defined as bot
+const Enmap = require('enmap');
+const EnmapLevel = require('enmap-level'); // Require the packages
 
 var config = require('./storages/config.json'); // Config File
 var guildConf = require('./storages/guildConf.json');
+
+client.settings = new Enmap({name: 'settings', persistent: true}); // create new settings
+
+const defaultSettings = {
+  modLogChannel: "mod-log"
+} // Sets the mod log, change it to whatever you like
+
+client.on("ready", ready => {
+  console.log("Bot launched beta edition")
+  client.guilds.forEach((g) => {
+    if (!client.settings.has(g.id)) {
+      client.settings.set(g.id, defaultSettings); // This checks if the guild has the settings, if it does not, create them.
+    }
+  });
+});
+
+client.on("guildCreate", guild => {
+  client.settings.set(guild.id, defaultSettings);
+}); // add the settings when the bot joins the guild
+
+// and finnaly, the actual command!
+
+client.on("message", (message) => {
+  if (message.content === "custom!setModLog") {
+  const thisConf = client.settings.get(message.guild.id); // gets the configuration
+thisConf.modLogChannel = `${message.channel.name}`; // sets the modLogChannel to the channel's name the message was sent in
+
+client.settings.set(message.guild.id, thisConf); // sets it
+message.channel.send("Sucessfuly changed mod logs!") // tells you it succeded
+}});
+
+// if you want an example command:
+client.on("message", (message) => {
+  if (message.content === "custom!ban") {
+  const thisConf = client.settings.get(message.guild.id);
+const channel = message.guild.channels.find('name', `${thisConf.modLogChannel}`); // Finds the channel
+channel.send("BEANED")
+}})
+
+
+
 
 bot.on('ready', () => { // If the Bot went on, proceed
     console.log('I\'m Online!');
